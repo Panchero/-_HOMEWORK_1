d98d4693f8571ae9f61819d9b3298fc4f7ad4667
Семен <marbledrop@yandex.ru>
2023-01-26 01:23:28 +0300

    add exercise_6

--- exercise_6/Program.cs
+++ exercise_6/Program.cs
@@ -2,3 +2,16 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Проверяем яаляется ли число четным");
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 2;
+if (result > 0)
+{
+ Console.WriteLine("число нечетное");
+}
+if (result == 0)
+{
+ Console.WriteLine("число четное");
+}
