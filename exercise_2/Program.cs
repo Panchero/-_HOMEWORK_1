97bc144b890e04747897a61c387cf337e1f0d43c
Семен <marbledrop@yandex.ru>
2023-02-02 01:08:43 +0300

    add exercise 2

--- exercise_2/Program.cs
+++ exercise_2/Program.cs
@@ -2,3 +2,19 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Нахождение большего числа из двух");
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+Console.WriteLine("Большее число " + number1);
+Console.WriteLine("Меньшее число " + number2);
+}
+else
+{
+Console.WriteLine("Большее число " + number2);
+Console.WriteLine("Меньшее число " + number1);
+}
